0ed1882c209067517e7df0ff3e0ceb01b1cca306
ロビヲ <73861392+robio-hell-near@users.noreply.github.com>
2020-11-04 11:43:09 +0900

    Add files via upload
    PC画面修正

--- web/js/main.js
+++ web/js/main.js
@@ -6,12 +6,23 @@ const koumoku3 = document.getElementById("koumoku3");
 const koumoku4 = document.getElementById("koumoku4");
 const koumoku5 = document.getElementById("koumoku5");
 const koumoku6 = document.getElementById("koumoku6");
+const koumoku10 = document.getElementById("koumoku10");
+const koumoku20 = document.getElementById("koumoku20");
+const koumoku30 = document.getElementById("koumoku30");
+const koumoku40 = document.getElementById("koumoku40");
+const koumoku50 = document.getElementById("koumoku50");
 const koumoku1close = document.getElementById("koumoku1close");
 const koumoku2close = document.getElementById("koumoku2close");
 const koumoku3close = document.getElementById("koumoku3close");
 const koumoku4close = document.getElementById("koumoku4close");
 const koumoku5close = document.getElementById("koumoku5close");
 const koumoku6close = document.getElementById("koumoku6close");
+const moku10 = document.getElementById("moku10");
+const moku20 = document.getElementById("moku20");
+const moku30 = document.getElementById("moku30");
+const moku40 = document.getElementById("moku40");
+const moku50 = document.getElementById("moku50");
+
 const back = document.getElementById("back");
 const n = 200;
 const topback = document.getElementById("topback");
@@ -81,4 +92,19 @@ koumoku5.addEventListener("click", function() {
 });
 koumoku6.addEventListener("click", function() {
    koumoku6close.classList.toggle("kopen"); 
+});
+koumoku10.addEventListener("click", function() {
+   moku10.classList.toggle("m20"); 
+});
+koumoku20.addEventListener("click", function() {
+   moku20.classList.toggle("m20"); 
+});
+koumoku30.addEventListener("click", function() {
+   moku30.classList.toggle("m20"); 
+});
+koumoku40.addEventListener("click", function() {
+   moku40.classList.toggle("m20"); 
+});
+koumoku50.addEventListener("click", function() {
+   moku50.classList.toggle("m20"); 
 });
